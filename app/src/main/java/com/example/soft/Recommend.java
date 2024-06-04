c5a151ffb5c661859942f258f4db56794b42020b
naminjae99 <slzhxlsthwn@naver.com>
2024-06-04 17:58:41 +0900

    06/04-2

--- app/src/main/java/com/example/soft/Recommend.java
+++ app/src/main/java/com/example/soft/Recommend.java
@@ -2,6 +2,9 @@ package com.example.soft;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,7 +19,7 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 import java.util.ArrayList;
 
 public class Recommend extends AppCompatActivity {
-    private  TextView textViewWeight;
+    private TextView textViewWeight;
     private BarChart barChart;
     private TextView textViewGraphDescription;
     private TextView textViewWeightChange;
@@ -24,9 +27,10 @@ public class Recommend extends AppCompatActivity {
     private float averageBMI;
     private float userBMI;
     private float averageNormalBMI;
-    private int height;
-    private int weight;
+    private float height;
+    private float weight;
     private TextView textbarchart;
+    private static final String TAG = "Recommend";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,20 +41,32 @@ public class Recommend extends AppCompatActivity {
         averageBMI = intent.getFloatExtra("averageBMI", 0);
         userBMI = intent.getFloatExtra("bmi", 0);
         averageNormalBMI = intent.getFloatExtra("averageNormalBMI", 0);
-        height = intent.getIntExtra("height", 0);
-        weight = intent.getIntExtra("weight", 0);
+        height = intent.getFloatExtra("height", 0);
+        weight = intent.getFloatExtra("weight", 0);
 
         // 막대 그래프 초기화
         barChart = findViewById(R.id.barChart);
 
         // 텍스트뷰 초기화
-        textbarchart=findViewById(R.id.textbarchart);
+        textbarchart = findViewById(R.id.textbarchart);
         textViewGraphDescription = findViewById(R.id.textViewGraphDescription);
-        textViewWeight=findViewById(R.id.textViewWeight);
-        textViewWeightChange=findViewById(R.id.textViewWeightChange);
+        textViewWeight = findViewById(R.id.textViewWeight);
+        textViewWeightChange = findViewById(R.id.textViewWeightChange);
         // 데이터 설정
         setChartData(averageBMI, userBMI, averageNormalBMI);
         displayWeightChange();
+
+        Button buttonNext = findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View v){
+                // 다음 화면으로 이동
+                Intent intent = new Intent(Recommend.this, GoodExerciseActivity.class);
+                intent.putExtra("name", name);
+
+                startActivity(intent);
+            }
+        });
     }
 
     private void setChartData(float averageBMI, float userBMI, float averageNormalBMI) {
@@ -60,7 +76,7 @@ public class Recommend extends AppCompatActivity {
         entries.add(new BarEntry(2, averageNormalBMI));
 
         BarDataSet dataSet = new BarDataSet(entries, "");
-        dataSet.setColors(new int[]{R.color.haneal, R.color.haneal, R.color.haneal}, this);
+        //dataSet.setColors(new int[]{R.color.haneal, R.color.haneal, R.color.haneal}, this);
         dataSet.setDrawValues(true); // 막대 그래프에 수치 표시
 
         BarData data = new BarData(dataSet);
@@ -75,29 +91,26 @@ public class Recommend extends AppCompatActivity {
         barChart.setDrawGridBackground(false);
 
         // X축 값 설정
-        final String[] xAxisLabels = new String[]{""};
+        final String[] xAxisLabels = new String[]{"평균 BMI", "사용자 BMI", "정상 평균 BMI"};
         barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(xAxisLabels));
 
         barChart.invalidate(); // 차트 다시 그리기
 
         // 텍스트뷰에 데이터 설정
-        textbarchart.setText(name+" 님의 동연령대 BMI 비교");
+        textbarchart.setText(name + " 님의 동연령대 BMI 비교");
         textViewGraphDescription.setText(name + "님의 권장 변화량");
     }
 
     private void displayWeightChange() {
-        if(userBMI <= averageNormalBMI){
-            textViewWeightChange.setText(name+"님은 정상입니다");
-        }
-        else{
-            for(int i=0;i<weight;i++){
-                float tmp=(weight-i)/(height * height) / 10000;
-                if(Math.abs(tmp-averageNormalBMI)<1){
-                    textViewWeightChange.setText(name+" 님의 당뇨병 예방을 위해 아래 항목이 권장됩니다.");
-                    textViewWeight.setText("1. 몸무게"+tmp+" kg 감량\\n2. 금연");
-                    break;
-                }
-            }
+        float normalWeightMin = userBMI * height * height / 10000;
+        float normalWeightMax = averageNormalBMI * height * height / 10000;
+
+        if (weight< normalWeightMax) {
+            textViewWeightChange.setText(name + "님은 정상입니다");
+            textViewWeight.setText("현재 체중을 유지하세요.");
+        } else if (weight >= normalWeightMax) {
+            textViewWeightChange.setText(name + " 님의 당뇨병 예방을 위해 아래 항목이 권장됩니다.");
+            textViewWeight.setText("1. 몸무게" +  String.format("%.1f", weight - normalWeightMax) + " kg 감량\n2. 금연");
         }
     }
 }
